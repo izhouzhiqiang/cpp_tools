0e89d5d2f5b0c630302d886096e27dd00a5f2737
firer1999 <zhouzhiqiang1116@163.com>
2024-05-07 08:00:18 -0700

    operators

--- src/string/string.cpp
+++ src/string/string.cpp
@@ -2,14 +2,19 @@
 #include <cstdlib>
 #include <cstring>
 
-string::string():p_data(nullptr),len(0)
-{
-    // pass
-};
-
 bool string::isNull () const
 {
     return p_data;
+}
+
+int string::length() const
+{
+    return len;
+}
+
+string::string():p_data(nullptr),len(0)
+{
+    // pass
 }
 
 string::string(const string& str)
@@ -27,8 +32,42 @@ string::string(const string& str)
     }
 }
 
-int string::length() const
+string::string(string&& str)
 {
-    return len;
+    len = str.len;
+    p_data = str.p_data;
+    str.len = 0;
+    str.p_data = nullptr;
 }
 
+string::string(const char* str) :len(0),p_data(nullptr)
+{
+    if(str)
+    {
+        len = strlen(str);
+        p_data = (char*)malloc(len + 1);
+    }
+}
+
+string::~string()
+{
+    if(p_data) free(p_data);
+}
+
+void string::concat(const string& str)
+{
+    // TO DO
+}
+
+string string::operator+(const string& str)
+{
+
+}
+string string::operator=(const string& str)
+{
+
+}
+bool string::operator==(const string& str)
+{
+    
+}
--- src/string/string.hpp
+++ src/string/string.hpp
@@ -2,14 +2,17 @@ class string{
     // string(const char *str);
 public:
     string(const string& str);
-    string(const string&& str);
-    string(const char *str,...);
+    string(string&& str);
+    string(const char *str);
     string();
     ~string();
     int length() const;
-    int find (const char* str);
-    int concat(const char* str);
+    int find(const string& str);
+    void concat(const string& str);
     bool isNull () const;
+    string operator+(const string& str);
+    string operator=(const string& str);
+    bool operator==(const string& str);
 private:
     char *p_data;
     int len;
